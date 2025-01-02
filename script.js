8ecc0a115643c8238defe679df07ad5aaaea284f
akatsuki600 <78849759+akatsuki600@users.noreply.github.com>
2025-01-02 10:52:16 +0500

    Layout and Styling Added layout and styling for the calculator, also added animation on click for buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+const answerScreen = document.querySelector("#answerScreen")
+const buttons = Array.from(document.querySelectorAll("#buttons > div"))
+const buttonsSection = document.querySelector("#buttonsSection")
+
+// buttonsSection.addEventListener("click",e => {
+//     console.log(e.target.textContent)
+// })
